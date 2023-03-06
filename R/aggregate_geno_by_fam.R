f3fb9306ffb1c018348d0a696096f960f805664e
loic mangnier <loic.mangnier.1@ulaval.ca>
2023-03-06 10:28:58 -0500

    correct check consanguinity

---
+++ R/aggregate_geno_by_fam.R
@@ -0,0 +1,43 @@
+# usethis::use_package("dplyr")
+# usethis::use_package("plyr")
+
+#'Aggregate genotypes among affected individuals for each family
+#'
+#'This function aggregates variants across family members. It takes a pedfile as first parameter.
+#'The six first columns must be family id, individual id, father and mother ids, sex and phenotype
+#'Homozyguous variants are replaced by their heterozyguous configurations.
+#'
+#'@param pedfile is a genotype file in ped format: A path to a .ped file
+#'@return A list with the ped file corrected and aggregated by family and index each variants observed in families
+#'@export
+
+agg.geno.by.fam = function(pedfile){
+  p = read.table(pedfile, header = F)
+  fam = p[,1:6]
+  affected = which(fam$V6==2)
+  genos = p[,7:ncol(p)]
+
+  genos[genos==1] = 0
+  genos[genos==2] = 1
+
+  df.genos = data.frame(do.call("cbind",lapply(seq(2, ncol(genos),2), function(x){
+    rowSums(genos[,c(x-1,x)])
+  })))
+
+  df.genos.affected = df.genos[affected,]
+  df.genos.affected = data.frame(t(unique(t(df.genos.affected))))
+  df.genos.affected[df.genos.affected==2] = 1
+  df.genos.affected = df.genos.affected[,-which(colSums(df.genos.affected)==0)]
+
+  df.genos.affected$pedigree = fam[affected,"V1"]
+
+  df.genos.agg.by.fam = aggregate(.~pedigree,df.genos.affected, sum)
+
+  index_null_fam = which(rowSums(df.genos.agg.by.fam[,-1])==0)
+  if(length(index_null_fam) > 0) df.genos.agg.by.fam = df.genos.agg.by.fam[-index_null_fam,]
+
+  locus.col = as.numeric(gsub("X", "", colnames(df.genos.agg.by.fam[,-1])))
+
+  return(list("ped_agg"=df.genos.agg.by.fam, "index_variants"=locus.col))
+
+}
--- R/check_consanguinity.R
+++ R/check_consanguinity.R
@@ -2,32 +2,60 @@
 
 #' Check whether there is consanguinity among pedigrees
 #'
-#' @param pedigree is a object of pedigree format
-#' 
+#' @param pedigree is a pedigree of a pedigreeList format or list of pedigrees
+#'
 
 .check.consanguinity = function(pedigree){
-  
-  ped.consanguinity = c()
-  
-  for(ped in 1:length(pedigree)){
-    df.ped = kinship2::as.data.frame.pedigree(pedigree[[ped]])
-    
-    couple = unique(df.ped[df.ped$dadid!=0,c("dadid","momid")])
-    kinship.mat = kinship2::kinship(pedigree[[ped]])
-    
-    consanguinity.coeff = c()
-    for(i in 1:nrow(couple)){
-      dadid = couple$dadid[i]
-      momid = couple$momid[i]
-      
-      consanguinity.coeff = c(consanguinity.coeff,kinship.mat[dadid,momid ])
-    }
-    
-    ped.consanguinity = c(ped.consanguinity, any(consanguinity.coeff>0))
-  }
-  
-  return(which(ped.consanguinity))
-}
 
+
+  if(class(pedigree) == "list"){
+    ped.consanguinity = c()
+
+    for(ped in 1:length(pedigree)){
+      df.ped = kinship2::as.data.frame.pedigree(pedigree[[ped]])
+
+      couple = unique(df.ped[df.ped$dadid!=0,c("dadid","momid")])
+      kinship.mat = kinship2::kinship(pedigree[[ped]])
+
+      consanguinity.coeff = c()
+      for(i in 1:nrow(couple)){
+        dadid = couple$dadid[i]
+        momid = couple$momid[i]
+
+        consanguinity.coeff = c(consanguinity.coeff,kinship.mat[dadid,momid ])
+      }
+
+      ped.consanguinity = c(ped.consanguinity, any(consanguinity.coeff>0))
+    }
+
+    return(which(ped.consanguinity))
+  }
+
+  else if (class(pedigree) == "pedigreeList"){
+
+    ped.consanguinity = c()
+    famid = unique(pedigree$famid)
+
+    for(ped in famid){
+      df.ped = kinship2::as.data.frame.pedigree(pedigree[ped])
+
+      couple = unique(df.ped[df.ped$dadid!=0,c("dadid","momid")])
+      kinship.mat = kinship2::kinship(pedigree[ped])
+
+      consanguinity.coeff = c()
+      for(i in 1:nrow(couple)){
+        dadid = as.character(couple$dadid[i])
+        momid = as.character(couple$momid[i])
+
+        consanguinity.coeff = c(consanguinity.coeff,kinship.mat[dadid,momid])
+      }
+
+      ped.consanguinity = c(ped.consanguinity, any(consanguinity.coeff>0))
+    }
+
+    return(which(ped.consanguinity))
+  }
+
+}
 
 
