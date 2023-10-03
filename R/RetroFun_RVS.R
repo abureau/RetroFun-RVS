77f7390e4a5db6cd0440b210412ee3fadb38fd6b
lmangnier <loic.mangnier@crchudequebec.ulaval.ca>
2023-10-03 14:40:55 -0400

    Add contributions by family and variant

--- R/RetroFun_RVS.R
+++ R/RetroFun_RVS.R
@@ -7,30 +7,45 @@
 #'@param Z_annot is a p*q matrix of functional annotations. The first column should be composed only with ones
 #'@param W is a vector of weights. Should be a unit vector when an unweighted approach is needed.
 #'@param independence is a boolean. Default value is FALSE. If variant independence can be assumed, use independence=TRUE.
-#'
+#'@param contrib.by.fam a boolean. If TRUE the function returns the weighted statistic by family
+#'@param contrib.by.var a boolean. If TRUE the function returns the weighted statistic by variant
 #'@return a data.frame of p-values for each score by annotation. ACAT and Fisher combined p-values are computed.
 #'@export
 
-RetroFun.RVS = function(null.value.by.fam, aggregate.geno.by.fam, Z_annot, W, independence=FALSE){
+RetroFun.RVS = function(null.value.by.fam, aggregate.geno.by.fam, Z_annot, W, independence=FALSE, contrib.by.fam=FALSE, contrib.by.var=FALSE){
 
   # if(.check.parameters.agg.null(null.value.by.fam, aggregate.geno.by.fam)%in%c(1,2)){
   #   warning("Correction parameters are not consistent with options provided for cryptic relatedness or consanguinity, please check ...")
   # }
 
-  Burden.Stat = compute.Burden.by.Annot(null.value.by.fam,aggregate.geno.by.fam,Z_annot,W)$B
+  run.Burden = compute.Burden.by.Annot(null.value.by.fam,aggregate.geno.by.fam,Z_annot,W, contrib.by.fam = contrib.by.fam, contrib.by.var = contrib.by.var)
+  Burden.Stat = run.Burden$B
   Var.Stat = unlist(compute.Var.by.Annot(null.value.by.fam,aggregate.geno.by.fam,Z_annot,W,independence = independence))
 
   p = pchisq(Burden.Stat/Var.Stat,1,lower.tail = FALSE)
 
   df.p=data.frame(p)
-#  colnames(df.p) = paste0("Score",1:length(p))
   colnames(df.p) = paste0("Score_",colnames(Z_annot))
-  
+
   df.p$ACAT = apply(df.p,1,function(x) ACAT::ACAT(x[!is.nan(x)]))
 
   df.p$Fisher = apply(df.p[,-ncol(df.p)],1, function(x){
     pchisq(-2*sum(log(x[!is.nan(x)])),2* length(x[!is.nan(x)]), lower.tail = F)
   })
 
-  return(df.p)
+  if(contrib.by.fam==TRUE & contrib.by.var==TRUE){
+    return(list("Resuts"=df.p, "Stat.by.fam" = run.Burden$Stat.by.fam , "Stat.by.var" = run.Burden$Stat.by.var))
+  }
+
+  else if(contrib.by.fam==TRUE & contrib.by.var==FALSE){
+    return(list("Resuts"=df.p, "Stat.by.fam" = run.Burden$Stat.by.fam ))
+  }
+
+  else if(contrib.by.fam==FALSE & contrib.by.var==TRUE){
+    return(list("Resuts"=df.p, "Stat.by.var" = run.Burden$Stat.by.var ))
+  }
+  else{
+    return(df.p)
+  }
+
 }
--- R/compute_Burden_by_Annot.R
+++ R/compute_Burden_by_Annot.R
@@ -9,11 +9,12 @@
 #'@param aggregate.geno.by.fam is the list returned by aggregate.geno.by.fam function
 #'@param Z_annot is a p*q matrix of functional annotations. The first column should be composed only with ones
 #'@param W is a vector of weights. Should be a unitary vector when unweighted version is needed.
-#'
+#'@param contrib.by.fam a boolean. If TRUE the function returns the weighted statistic by family
+#'@param contrib.by.var a boolean. If TRUE the function returns the weighted statistic by variant
 #'@return a list with each score by annotation
 #'@export
 #'
-compute.Burden.by.Annot = function(null.value.by.fam,aggregate.geno.by.fam,Z_annot,W){
+compute.Burden.by.Annot = function(null.value.by.fam,aggregate.geno.by.fam,Z_annot,W, contrib.by.fam=FALSE, contrib.by.var=FALSE){
 
   #Replace NA by zero in Z
   Z_annot[is.na(Z_annot)] = 0
@@ -32,10 +33,10 @@ compute.Burden.by.Annot = function(null.value.by.fam,aggregate.geno.by.fam,Z_ann
   }
 
   ped_agg = aggregate.geno.by.fam$ped_agg
-  
+
   W_mat = diag(W, nrow=length(W), ncol=length(W))
   W_sub = W_mat[aggregate.geno.by.fam$index_variants,aggregate.geno.by.fam$index_variants]
-  
+
   Expected = null.value.by.fam[,c("FamID", "Expected")]
 
   split_G_agg_by_fam = split(ped_agg, 1:nrow(ped_agg))
@@ -51,13 +52,37 @@ compute.Burden.by.Annot = function(null.value.by.fam,aggregate.geno.by.fam,Z_ann
   diff_obs_expected_all_fam = do.call("rbind", diff_obs_expected_by_fam)
   diff_obs_expected_all_fam[is.na(diff_obs_expected_all_fam)] = 0
 
+  #Sum over family for each variant
   S_by_var = colSums(diff_obs_expected_all_fam)
+
   Wz_sub = W_sub%*%Z_annot[aggregate.geno.by.fam$index_variants,]
 
+  #Total Contribution by family across functional annotation
+  Stat_by_fam = rowSums(as.matrix(diff_obs_expected_all_fam)%*%Wz_sub)
+  names(Stat_by_fam) = ped_agg$pedigree
 
+  #Total Contribution by variant across functional annotation
+  Stat_by_var = sapply(1:length(S_by_var), function(x) sum(S_by_var[x]*Wz_sub[x,]))
+  names(Stat_by_var) = colnames(ped_agg)[-1]
+  #Contribution for each functional annotation
   S_Wz = S_by_var%*%Wz_sub
+
   Burden_by_annot = S_Wz^2
 
-  return(list("B"=Burden_by_annot))
+  if(contrib.by.fam==TRUE & contrib.by.var==TRUE){
+    return(list("B"=Burden_by_annot, "Stat.by.fam" = Stat_by_fam , "Stat.by.var" = Stat_by_var))
+  }
+
+  else if(contrib.by.fam==TRUE & contrib.by.var==FALSE){
+    return(list("B"=Burden_by_annot, "Stat.by.fam" = Stat_by_fam ))
+  }
+
+  else if(contrib.by.fam==FALSE & contrib.by.var==TRUE){
+    return(list("B"=Burden_by_annot, "Stat.by.var" = Stat_by_var ))
+  }
+  else{
+    return(list("B"=Burden_by_annot))
+  }
+
 
 }
