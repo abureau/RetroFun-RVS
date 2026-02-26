fccfb510877b2d0f5011d4bd54d36c6a1172c7c2
abureau <abureau6@gmail.com>
2026-02-26 16:28:51 -0500

    Cleaning replace_covar_by_fam.R

--- R/replace_covar_by_fam.R
+++ R/replace_covar_by_fam.R
@@ -10,9 +10,6 @@
 replace.covar.by.fam = function(null.value.by.fam,null.covar.by.indiv)
 {
   # Calcule 2 x la somme des covariances individuelles
-  #covar.by.fam = sapply(null.covar.by.indiv,function(covar) 2*sum(covar$`Covar(ij,i'j')`[lower.tri(covar$`Covar(ij,i'j')`)])+sum(diag(covar$`Covar(ij,i'j')`)))
-  # Cette version prend 2 fois la somme de la matrice et soustrait la somme de la diagonale qu'il faut compter une seule fois.
-  # Ça évite d'extraire la diagonale inférieure.
   covar.by.fam = sapply(null.covar.by.indiv,function(covar) sum(covar$`Covar(ij,i'j')`))
   names(covar.by.fam) = sapply(null.covar.by.indiv,function(l) l$FamId)
   null.value.by.fam[,"CoVar"] = covar.by.fam[null.value.by.fam$FamID]
